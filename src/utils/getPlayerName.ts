b5cf6640e22f2b0af12e60df5220dafc8a58b0c4
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2022-04-22 17:18:04 +0700

    Fixed process of several squad prefixes

--- src/utils/getPlayerName.ts
+++ src/utils/getPlayerName.ts
@@ -1,7 +1,7 @@
 const getPlayerName = (playerName: PlayerName): [PlayerName, PlayerPrefix | null] => {
   if (!playerName.includes('[')) return [playerName, null];
 
-  const squadPrefixRegex = /\[\w*\]/;
+  const squadPrefixRegex = /\[.*\]/;
   const squadPrefix = playerName.match(squadPrefixRegex)[0];
   const name = playerName.replace(squadPrefixRegex, '');
 
