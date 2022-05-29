6f2c92d3793bfcb8f707214ff52e237f13e3966c
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2022-05-29 15:48:29 +0700

    Fixed duplicate replays

--- src/replays/getReplays.ts
+++ src/replays/getReplays.ts
@@ -1,8 +1,11 @@
+import uniqBy from 'lodash/uniqBy';
+
 import fetchData from '../fetchData';
 
 const getReplays = async (gameType: GameType): Promise<Replay[]> => {
   const allReplays = await fetchData<ReplayRaw[]>('https://replays.solidgames.ru/Replays');
-  const replays = allReplays.filter(
+  const uniqueReplays = uniqBy(allReplays, 'filename');
+  const replays = uniqueReplays.filter(
     (replay) => (
       replay.mission_name.startsWith(gameType)
       && !replay.mission_name.startsWith('sgs')
