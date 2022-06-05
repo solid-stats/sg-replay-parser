9d13b8f9e95918dcddce4921b5b187efcf734ad4
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2022-06-05 16:53:23 +0700

    Fixed wront return

--- src/0 - utils/calculateKDRatio.ts
+++ src/0 - utils/calculateKDRatio.ts
@@ -5,7 +5,7 @@ const calculateKDRatio = (
   teamkills: GlobalPlayerStatistics['teamkills'],
   deaths: GlobalPlayerStatistics['deaths'],
 ): GlobalPlayerStatistics['kdRatio'] => {
-  const deathsWithoutByTeamkills = deaths.total - deaths.byTeamkills;
+  const deathsWithoutByTeamkills = Math.abs(deaths.total - deaths.byTeamkills);
 
   if (!deathsWithoutByTeamkills) return kills;
 
