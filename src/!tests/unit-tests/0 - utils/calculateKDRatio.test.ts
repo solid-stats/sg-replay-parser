28f4e8da7b9ff7dfe67cb5ea3cdb8519811d6e84
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2022-08-01 15:39:59 +0700

    Returned tests

---
+++ src/!tests/unit-tests/0 - utils/calculateKDRatio.test.ts
@@ -0,0 +1,109 @@
+import calculateKDRatio from '../../../0 - utils/calculateKDRatio';
+
+type TestData = {
+  kills: Kills;
+  teamkills: Teamkills;
+  deaths: Deaths;
+  result: number;
+};
+
+const tests: TestData[] = [
+  {
+    kills: 0,
+    teamkills: 0,
+    deaths: {
+      total: 0,
+      byTeamkills: 0,
+    },
+    result: 0,
+  },
+  {
+    kills: 5,
+    teamkills: 0,
+    deaths: {
+      total: 0,
+      byTeamkills: 0,
+    },
+    result: 5,
+  },
+  {
+    kills: 3,
+    teamkills: 0,
+    deaths: {
+      total: 2,
+      byTeamkills: 0,
+    },
+    result: 1.5,
+  },
+  {
+    kills: 5,
+    teamkills: 2,
+    deaths: {
+      total: 2,
+      byTeamkills: 0,
+    },
+    result: 1.5,
+  },
+  {
+    kills: 3,
+    teamkills: 0,
+    deaths: {
+      total: 3,
+      byTeamkills: 1,
+    },
+    result: 1.5,
+  },
+  {
+    kills: 3,
+    teamkills: 1,
+    deaths: {
+      total: 3,
+      byTeamkills: 1,
+    },
+    result: 1,
+  },
+  {
+    kills: 7,
+    teamkills: 0,
+    deaths: {
+      total: 0,
+      byTeamkills: 2,
+    },
+    result: 3.5,
+  },
+  {
+    kills: 0,
+    teamkills: 0,
+    deaths: {
+      total: 2,
+      byTeamkills: 0,
+    },
+    result: 0,
+  },
+  {
+    kills: 0,
+    teamkills: 2,
+    deaths: {
+      total: 2,
+      byTeamkills: 0,
+    },
+    result: -1,
+  },
+  {
+    kills: 0,
+    teamkills: 2,
+    deaths: {
+      total: 2,
+      byTeamkills: 1,
+    },
+    result: -2,
+  },
+];
+
+tests.map(({
+  kills, teamkills, deaths, result,
+}) => (
+  test(`${kills} kills, ${teamkills} teamkills, ${deaths.total} deaths and ${deaths.byTeamkills} deaths by teamkills. Should return ${result} K/D`, () => {
+    expect(calculateKDRatio(kills, teamkills, deaths)).toBe(result);
+  })
+));
---
+++ src/!tests/unit-tests/0 - utils/calculateScore.test.ts
@@ -0,0 +1,74 @@
+import calculateScore from '../../../0 - utils/calculateScore';
+
+type TestData = {
+  playedGames: number;
+  kills: Kills;
+  teamkills: Teamkills;
+  deaths: Deaths;
+  result: number;
+};
+
+const total = 0;
+const defaultValues: TestData = {
+  playedGames: 50,
+  kills: 100,
+  teamkills: 0,
+  deaths: {
+    total,
+    byTeamkills: 0,
+  },
+  result: 2,
+};
+const tests: TestData[] = [
+  defaultValues,
+  {
+    ...defaultValues,
+    teamkills: 7,
+    result: 1.86,
+  },
+  {
+    ...defaultValues,
+    teamkills: 7,
+    deaths: {
+      total,
+      byTeamkills: 12,
+    },
+    result: 2.45,
+  },
+  {
+    ...defaultValues,
+    deaths: {
+      total,
+      byTeamkills: 12,
+    },
+    result: 2.63,
+  },
+  {
+    playedGames: 2,
+    kills: 0,
+    teamkills: 0,
+    deaths: {
+      byTeamkills: 2,
+      total: 0,
+    },
+    result: 0,
+  },
+  {
+    playedGames: 2,
+    kills: 4,
+    teamkills: 0,
+    deaths: {
+      byTeamkills: 2,
+      total: 0,
+    },
+    result: 4,
+  },
+];
+
+tests.map(({
+  playedGames, kills, teamkills, deaths, result,
+}) => (
+  test(`${playedGames} played games, ${kills} kills, ${teamkills} teamkills and ${deaths.byTeamkills} deaths by teamkills. Should return ${result} score`, () => {
+    expect(calculateScore(playedGames, kills, teamkills, deaths)).toBe(result);
+  })
+));
