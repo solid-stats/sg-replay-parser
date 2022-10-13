9f973ddac3ae56987bd9ac738a02bed0b71eea18
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2022-10-13 19:29:24 +0700

    Fix sort

--- src/3 - statistics/global/index.ts
+++ src/3 - statistics/global/index.ts
@@ -24,6 +24,18 @@ const limitWeaponsStatisticsCount = (
   }))
 );
 
+const limitOtherPlayersStatsCount = (
+  statistics: GlobalPlayerStatistics[],
+): GlobalPlayerStatistics[] => (
+  statistics.map((playerStatistics) => ({
+    ...playerStatistics,
+    killed: orderBy(playerStatistics.killed, 'count', 'desc').slice(0, 25),
+    killers: orderBy(playerStatistics.killers, 'count', 'desc').slice(0, 25),
+    teamkilled: orderBy(playerStatistics.teamkilled, 'count', 'desc').slice(0, 25),
+    teamkillers: orderBy(playerStatistics.teamkillers, 'count', 'desc').slice(0, 25),
+  }))
+);
+
 const calculateGlobalStatistics = (
   replays: PlayersGameResult[],
 ): GlobalPlayerStatistics[] => {
@@ -42,6 +54,7 @@ const calculateGlobalStatistics = (
   const resultStatistics = pipe(
     sortPlayerStatistics,
     limitWeaponsStatisticsCount,
+    limitOtherPlayersStatsCount,
   )(globalStatistics);
 
   return resultStatistics;
