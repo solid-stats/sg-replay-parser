959578cfc5d889a66798d1ad2131530d7e2fc5e2
Ilya Mityaev <78545085+police1337228@users.noreply.github.com>
2023-04-09 01:30:17 +0300

    Added comma to the rotations file

--- src/0 - utils/rotations.ts
+++ src/0 - utils/rotations.ts
@@ -13,7 +13,7 @@ const startDates: string[] = [
   '2022-07-04T00:00:00.000Z',
   '2022-10-03T00:00:00.000Z',
   '2023-01-09T00:00:00.000Z',
-  '2023-04-07T00:00:00.000Z'
+  '2023-04-07T00:00:00.000Z',
 ];
 
 const rotationsStartDates: Dayjs[] = startDates.map((startDate) => (
