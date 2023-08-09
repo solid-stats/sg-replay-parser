2fa266ed582745d4b2f48a1258453265e00faa6d
Alexandr <pavlov_aleksandr_2001@mail.ru>
2023-08-10 03:24:43 +0700

    Updated replays parsing after website update

--- src/!prepareReplaysList/parseReplaysOnPage.ts
+++ src/!prepareReplaysList/parseReplaysOnPage.ts
@@ -4,24 +4,6 @@ import pLimit from 'p-limit';
 import { dayjsUnix } from '../0 - utils/dayjs';
 import parseReplay from './parseReplay';
 
-// mission game type protected by CloudFlare email obfuscation because contains '@' sign
-// this is bug and should be fixed
-// untill there is no fix, we use this decode function
-// got this from here: https://usamaejaz.com/cloudflare-email-decoding/
-const decodeMissionGameType = (encodedMissionGameType: string) => {
-  let result = '';
-  const r = parseInt(encodedMissionGameType.substr(0, 2), 16);
-
-  for (let i = 2; encodedMissionGameType.length - i; i += 2) {
-    // eslint-disable-next-line no-bitwise
-    const charCode = parseInt(encodedMissionGameType.substr(i, 2), 16) ^ r;
-
-    result += String.fromCharCode(charCode);
-  }
-
-  return result;
-};
-
 const parseTableRowInfo = async (
   el: Element,
   alreadyParsedReplays: Output['parsedReplays'],
@@ -36,24 +18,20 @@ const parseTableRowInfo = async (
 
   if (alreadyParsedReplays.includes(replayLink) || excludeReplays.includes(replayLink)) return null;
 
-  // regexp removes [email protected] from string
-  const missionName = linkElement.textContent.replace(/\[[^\]]*\]*/g, '');
-  const encodedMissionsGameType = linkElement.querySelector('span')?.getAttribute('data-cfemail');
-  let missionGameType: string | null = null;
-
+  const missionInfo = linkElement.textContent.split('@');
+  let missionGameType = missionInfo[0];
+  const missionName = missionInfo[1];
   const replayToInclude = includeReplays.find(({ name }) => (name === missionName));
 
-  if (encodedMissionsGameType) missionGameType = decodeMissionGameType(encodedMissionsGameType);
+  if (replayToInclude && !missionGameType) missionGameType = `${replayToInclude.gameType}@`;
 
-  if (replayToInclude && !encodedMissionsGameType) missionGameType = `${replayToInclude.gameType}@`;
-
-  if (missionGameType === null) return null;
+  if (!missionGameType) return null;
 
   const filename = await parseReplay(replayLink);
   const date = dayjsUnix(parseInt(replayLink.split('/')[2], 10)).toJSON();
 
   return {
-    mission_name: missionGameType + missionName,
+    mission_name: `${missionGameType}@${missionName}`,
     filename,
     date,
     serverId: parseInt(tableCells[2].textContent || '', 10) || 0,
