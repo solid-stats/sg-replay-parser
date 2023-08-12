c685fd7b34003f724c64453674d413aff4298014
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2023-08-12 16:30:11 +0700

    Fixed linter

--- src/0 - utils/isInInterval.ts
+++ src/0 - utils/isInInterval.ts
@@ -1,5 +1,10 @@
 import { Dayjs } from 'dayjs';
 
-export const isInInterval = (date: Dayjs, startDate: Dayjs, endDate: Dayjs, excludeEndDate?: boolean): boolean => (
+export const isInInterval = (
+  date: Dayjs,
+  startDate: Dayjs,
+  endDate: Dayjs,
+  excludeEndDate?: boolean,
+): boolean => (
   date.isBetween(startDate, endDate, 'ms', excludeEndDate ? '[)' : '[]')
 );
