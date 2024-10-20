0e62b2cd4402c846adba894cc12a0da512a95fe3
Pavlov Alexandr <pavlov_aleksandr_2001@mail.ru>
2024-10-20 07:57:17 +0700

    Added new rotation

--- src/0 - utils/rotations.ts
+++ src/0 - utils/rotations.ts
@@ -18,6 +18,7 @@ const startDates: string[] = [
   '2023-10-02T00:00:00.000Z',
   '2024-04-08T00:00:00.000Z',
   '2024-07-01T00:00:00.000Z',
+  '2024-10-01T00:00:00.000Z',
 ];
 
 const rotationsStartDates: Dayjs[] = startDates.map((startDate) => (
